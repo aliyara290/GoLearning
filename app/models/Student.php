6be9886045ce284e1368178b54532b00e3e079db
Ali Yara <ali.yara.fr@gmail.com>
2025-01-18 22:23:53 +0100

    student registration

--- app/models/Student.php
+++ app/models/Student.php
@@ -1,13 +1,23 @@
 <?php 
+namespace App\Models;
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-// namespace App\Models;
-// use App\Models\User;
+use App\Models\User;
+class Student extends User {
+    public function __construct(
+        string $firstName,
+        string $lastName,
+        string $email,
+        string $username,
+        string $password,
+    )
+    {
+        parent::__construct("student", $firstName, $lastName, $email, $username, $password);
+        parent::register();
+    }
 
-// class Student extends User {
-//     private array $courseEnrolled;
-
-//     public function enrollInCourse() {
+    // public function viewProfile()
+    // {
         
-//     }
-   
-// }
+    // }
+}
