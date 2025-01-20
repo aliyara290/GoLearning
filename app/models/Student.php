eeb9fa319029551538b2a1801f2782ea802dcd2c
Ali Yara <ali.yara.fr@gmail.com>
2025-01-20 15:26:18 +0100

    add enrollInCourse method

--- app/models/Student.php
+++ app/models/Student.php
@@ -3,21 +3,23 @@ namespace App\Models;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Models\User;
-class Student extends User {
-    public function __construct(
-        string $firstName,
-        string $lastName,
-        string $email,
-        string $username,
-        string $password,
-    )
-    {
-        parent::__construct("student", $firstName, $lastName, $email, $username, $password);
-        parent::register();
-    }
+use PDOException;
 
-    // public function viewProfile()
-    // {
-        
-    // }
+class Student extends User {
+    
+    public function enrollInCourse($student_id, $course_id) {
+        $sql = "INSERT INTO enrollments (student_id, course_id) VALUES (:student_id, :course_id)";
+        $data = [
+            ":student_id" => $student_id,
+            ":course_id" => $course_id
+        ];
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($data);
+            return true;
+        } catch(PDOException $error) {
+            echo "failed to insert data in enrollements table: ". $error->getMessage();
+            return false;
+        }
+    }
 }
