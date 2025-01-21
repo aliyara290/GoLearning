99536721bac019d86a8dd76bfa2a54e32dcdc536
Ali Yara <ali.yara.fr@gmail.com>
2025-01-21 23:06:02 +0100

    add enrollInCourse/isEnrolled/viewMyCourse methods

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -8,21 +8,29 @@ use App\Models\Student;
 
 class StudentController
 {
-    private $teacherModel;
+    private $studentModel;
 
     public function __construct()
     {
-        $this->teacherModel = new Student();
+        $this->studentModel = new Student();
     }
 
     public function enrollInCourse() {
         if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["courseId"], $_POST["studentId"])) {
             $student_id = $_POST["studentId"];
             $course_id = $_POST["courseId"];
-            $check = $this->teacherModel->enrollInCourse($student_id, $course_id);
+            $check = $this->studentModel->enrollInCourse($student_id, $course_id);
             if($check) {
                 header("location: /app/views/front/learn.php?courseId=$course_id");
             } else echo "failed";
         }
+    }
+
+    public function isEnrolled($student_id, $course_id) {
+        return $this->studentModel->isEnrolled($student_id, $course_id);
+    }
+    public function viewMyCourses() {
+        $student_id = $_SESSION["user"]["userId"];
+        return $this->studentModel->viewMyCourses($student_id);
     }
 }
